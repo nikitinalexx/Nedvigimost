99ad45386b960add6649130a7fc5d640a34af480
Алексей <nikitinalexx22@gmail.com>
2016-02-14 16:15:12 +0200

    validations

--- src/main/java/com/nedvigimost/Main.java
+++ src/main/java/com/nedvigimost/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         ApplicationContext ctx = new ClassPathXmlApplicationContext("SpringBean.xml");
-        Performer p = ctx.getBean("kenny", Performer.class);
-        p.perform();
+        //Performer p = ctx.getBean("kenny", Performer.class);
+        //p.perform();
     }
 }
--- src/main/java/com/nedvigimost/vo/Photo.java
+++ src/main/java/com/nedvigimost/vo/Photo.java
@@ -1,7 +1,48 @@
 package com.nedvigimost.vo;
 
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by Алексей on 14.02.2016.
  */
+@Component
 public class Photo {
+    private int id;
+    @NotNull
+    private String path;
+    @NotNull
+    private int idBuilding;
+
+    public Photo() {}
+
+    public Photo(String path, int idBuilding) {
+        this.path = path;
+        this.idBuilding = idBuilding;
+    }
+
+    public int getIdBuilding() {
+        return idBuilding;
+    }
+
+    public void setIdBuilding(int idBuilding) {
+        this.idBuilding = idBuilding;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
