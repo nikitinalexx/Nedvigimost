dff3af04fbec7b528c4abc4579eac6b3bb7b830a
Malkevych Bohdan Ihorovych <Bohdan@MacBook-Pro-oivrafiv-2.local>
2016-02-14 16:39:00 +0200

    Implemented Buiding and Order

--- src/main/java/com/nedvigimost/vo/Building.java
+++ src/main/java/com/nedvigimost/vo/Building.java
@@ -1,7 +1,183 @@
 package com.nedvigimost.vo;
 
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
- * Created by Алексей on 14.02.2016.
+ * Created by пїЅпїЅпїЅпїЅпїЅпїЅпїЅ on 14.02.2016.
  */
+@Component
 public class Building {
+
+    private int idBuilding;
+    @Min(1) @Max(2)
+    private int type; // 1 is for selling
+    @Min(1)
+    private int numOfFloors;
+    @Min(1)
+    private int numOfRooms;
+    @Min(1)
+    private int numOfBuths;
+    @Min(0)
+    private double livingArea;
+    @Min(0)
+    private double totalArea;
+    @Min(0)
+    private double courtArea;
+    @Min(0)
+    private double distanceToMetro;
+    @NotNull @Size(min=1, max=255)
+    private String address;
+    private Boolean isAnimalsAllowed;
+    @NotNull
+    private int idOwner;
+
+    private String notes;
+
+
+
+    public void setIdBuilding(int idBuilding) {
+        this.idBuilding = idBuilding;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setNumOfFloors(int numOfFloors) {
+        this.numOfFloors = numOfFloors;
+    }
+
+    public void setNumOfRooms(int numOfRooms) {
+        this.numOfRooms = numOfRooms;
+    }
+
+    public void setNumOfBuths(int numOfBuths) {
+        this.numOfBuths = numOfBuths;
+    }
+
+    public void setLivingArea(double livingArea) {
+        this.livingArea = livingArea;
+    }
+
+    public void setTotalArea(double totalArea) {
+        this.totalArea = totalArea;
+    }
+
+    public void setCourtArea(double courtArea) {
+        this.courtArea = courtArea;
+    }
+
+    public void setDistanceToMetro(double distanceToMetro) {
+        this.distanceToMetro = distanceToMetro;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setAnimalsAllowed(Boolean animalsAllowed) {
+        isAnimalsAllowed = animalsAllowed;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setIdOwner(int idOwner) {
+        this.idOwner = idOwner;
+    }
+
+
+    public Building() {
+    }
+
+    public Building(int type, int numOfFloors, int numOfRooms, int numOfBuths, double livingArea, double totalArea, double courtArea, double distanceToMetro, String address, Boolean isAnimalsAllowed, String notes, int idOwner) {
+        this.type = type;
+        this.numOfFloors = numOfFloors;
+        this.numOfRooms = numOfRooms;
+        this.numOfBuths = numOfBuths;
+        this.livingArea = livingArea;
+        this.totalArea = totalArea;
+        this.courtArea = courtArea;
+        this.distanceToMetro = distanceToMetro;
+        this.address = address;
+        this.isAnimalsAllowed = isAnimalsAllowed;
+        this.notes = notes;
+        this.idOwner = idOwner;
+    }
+
+    public Building(int idBuilding, int type, int numOfFloors, int numOfRooms, int numOfBuths, double livingArea, double totalArea, double courtArea, double distanceToMetro, String address, Boolean isAnimalsAllowed, String notes, int idOwner) {
+        this.idBuilding = idBuilding;
+        this.type = type;
+        this.numOfFloors = numOfFloors;
+        this.numOfRooms = numOfRooms;
+        this.numOfBuths = numOfBuths;
+        this.livingArea = livingArea;
+        this.totalArea = totalArea;
+        this.courtArea = courtArea;
+        this.distanceToMetro = distanceToMetro;
+        this.address = address;
+        this.isAnimalsAllowed = isAnimalsAllowed;
+        this.notes = notes;
+        this.idOwner = idOwner;
+    }
+
+    public int getIdBuilding() {
+        return idBuilding;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getNumOfFloors() {
+        return numOfFloors;
+    }
+
+    public int getNumOfRooms() {
+        return numOfRooms;
+    }
+
+    public int getNumOfBuths() {
+        return numOfBuths;
+    }
+
+    public double getLivingArea() {
+        return livingArea;
+    }
+
+    public double getTotalArea() {
+        return totalArea;
+    }
+
+    public double getCourtArea() {
+        return courtArea;
+    }
+
+    public double getDistanceToMetro() {
+        return distanceToMetro;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Boolean getAnimalsAllowed() {
+        return isAnimalsAllowed;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public int getIdOwner() {
+        return idOwner;
+    }
+
+
 }
--- src/main/java/com/nedvigimost/vo/Order.java
+++ src/main/java/com/nedvigimost/vo/Order.java
@@ -1,7 +1,127 @@
 package com.nedvigimost.vo;
 
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import java.util.Calendar;
+
 /**
- * Created by Алексей on 14.02.2016.
+ * Created by пїЅпїЅпїЅпїЅпїЅпїЅпїЅ on 14.02.2016.
  */
+@Component
 public class Order {
+
+    private int idOrder;
+    @Min(1) @Max(2)
+    private int type;  // 1 is for selling
+    @Min(0)
+    private int idBuilding;
+    @Min(0)
+    private int idOwner;
+    @Min(0)
+    private int idClient;
+    @Min(0)
+    private int price;
+
+
+    public Order() {
+    }
+
+    public Order(int type, int idBuilding, int idOwner, int idClient, Calendar startDate, Calendar endDate, int price) {
+        this.type = type;
+        this.idBuilding = idBuilding;
+        this.idOwner = idOwner;
+        this.idClient = idClient;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.price = price;
+    }
+
+    @Past
+    private Calendar startDate;
+
+    public Order(int idOrder, int type, int idBuilding, int idOwner, int idClient, Calendar startDate, Calendar endDate, int price) {
+        this.idOrder = idOrder;
+        this.type = type;
+        this.idBuilding = idBuilding;
+        this.idOwner = idOwner;
+        this.idClient = idClient;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.price = price;
+    }
+
+    @Past
+    private Calendar endDate;
+
+
+
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(int idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getIdBuilding() {
+        return idBuilding;
+    }
+
+    public void setIdBuilding(int idBuilding) {
+        this.idBuilding = idBuilding;
+    }
+
+    public int getIdOwner() {
+        return idOwner;
+    }
+
+    public void setIdOwner(int idOwner) {
+        this.idOwner = idOwner;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public Calendar getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Calendar startDate) {
+        this.startDate = startDate;
+    }
+
+    public Calendar getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Calendar endDate) {
+        this.endDate = endDate;
+    }
+
 }
